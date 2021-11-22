157202e71513f0f50cf84706b0f836ae5fdfb59c
linpu <im.linpu@outlook.com>
2021-11-22 11:18:50 +0800

    mvc

--- src/main/java/com/lin/demo/juc/sut01/collection/NotSafeDemo.java
+++ src/main/java/com/lin/demo/juc/sut01/collection/NotSafeDemo.java
@@ -1,7 +1,6 @@
 package com.lin.demo.juc.sut01.collection;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
@@ -20,13 +19,13 @@ import java.util.UUID;
  * @version v1.0
  * @program juc
  * @description
- * @date 2021-03-31 00:50
  */
+@SuppressWarnings("all")
 public class NotSafeDemo {
     public static void main(String[] args) {
-//        List<String> list = new ArrayList<>();
+        List<String> list = new ArrayList<>();
 //        List<String> list = new Vector<>();
-        List<String> list = Collections.synchronizedList(new ArrayList<>());//CRUD加了synchronized;迭代器没有加锁
+//        List<String> list = Collections.synchronizedList(new ArrayList<>());//CRUD加了synchronized;迭代器没有加锁
 
         //ArrayList在多个线程同时对其进行修改的时候，
         // 就会抛出**java.util.ConcurrentModificationException异常（并发修改异常）**，
