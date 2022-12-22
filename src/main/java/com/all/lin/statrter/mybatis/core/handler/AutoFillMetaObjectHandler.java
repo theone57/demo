1b68499a4e9b8d0180270932925c457711fba6d4
theone57 <im.linpu@outlook.com>
2022-12-22 10:42:40 +0800

    docs(AutoFillMetaObjectHandler)：增加备注多数据源需要手动设置 自动填充

--- src/main/java/com/all/lin/statrter/mybatis/core/handler/AutoFillMetaObjectHandler.java
+++ src/main/java/com/all/lin/statrter/mybatis/core/handler/AutoFillMetaObjectHandler.java
@@ -9,9 +9,9 @@ import java.time.LocalDateTime;
  * @author linpu
  * @dateTime 2022-07-28 09:29
  * @email im.linpu@qq.com
- * @description
+ * @description 配置自定义多数据源 需要手动设置config里
  */
-public class AutoFillMetaObjectHandler  implements MetaObjectHandler {
+public class AutoFillMetaObjectHandler implements MetaObjectHandler {
     @Override
     public void insertFill(MetaObject metaObject) {
         //第一个对应实体属性名, 第二个参数需要填充的值
